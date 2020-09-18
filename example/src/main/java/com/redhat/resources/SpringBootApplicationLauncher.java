219fe10c781560fa527cca3c18271bccece00605
João Paulo Maida <jpmaida@gmail.com>
2020-09-18 14:02:12 -0300

    First commit: Estrutura de projeto maven web + Hello World Spring Boot

---
+++ example/src/main/java/com/redhat/resources/SpringBootApplicationLauncher.java
@@ -0,0 +1,9 @@
+package com.redhat.resources;
+
+import org.springframework.boot.SpringApplication;
+
+public class SpringBootApplicationLauncher {
+    public static void main(String[] args) {
+        SpringApplication.run(PingPong.class, args);
+    }
+}
