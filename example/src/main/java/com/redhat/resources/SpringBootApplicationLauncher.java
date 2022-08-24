3e9e5f6d3d8dacd0c7e279ec55811a8a4e1f672e
João Paulo Maida <jpmaida@gmail.com>
2022-08-23 23:04:05 -0300

    Retirada de diretório example

--- example/src/main/java/com/redhat/resources/SpringBootApplicationLauncher.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.resources;
-
-import org.springframework.boot.SpringApplication;
-
-public class SpringBootApplicationLauncher {
-    public static void main(String[] args) {
-        SpringApplication.run(PingPong.class, args);
-    }
-}
